2063b787f3ebebe9fb3b1936dd6a5f1ef4dc5f0a
arnav-sys <programmeraggarwal@gmail.com>
2021-06-25 16:28:50 +0530

    make it better

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,25 +8,12 @@ const Footer = () => {
       Made with <HeartTwoTone twoToneColor="#eb2f96" /> By{" "}
       <a
         className="footer__link"
-        href="https://github.com/mihir0699/Video-Chat"
+        href="https://portfolio-website-df20d.web.app/"
         target="_blank"
         style={{ color: "#fdfdfd" }}
         rel="noreferrer"
       >
-        Mihir Gupta
-      </a>
-      <br />
-
-      Contributed/Improved By{" "}
-
-      <a
-        className="footer__link"
-        href="https://linkedin.com/in/gautamtiwari003"
-        target="_blank"
-        style={{ color: "#fdfdfd" }}
-        rel="noreferrer"
-      >
-        Gautam Tiwari
+        arnav aggrwal
       </a>
     </footer>
   );
